c06e52079c408f9ce982af167b7ad2bb15d9ec89
paballom <molelekipaballo@gmail.com>
2023-08-10 03:03:08 -0700

    a C program that prints exactly with a use of of  the function puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#inlude <stdio.h>
+/**
+ *main-entry point 
+ *return: always  0 (success)
+ */
+int main(void)
+{
+puts( "Programming is like building a multilingual puzzle" );
+      return ( 0 );
+}
