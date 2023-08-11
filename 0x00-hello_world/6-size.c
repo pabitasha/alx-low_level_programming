35913189fff7ba31471fee91e53656df6b067b91
paballom <molelekipaballo@gmail.com>
2023-08-11 02:48:01 -0700

    This program will assign a random number to the variable n each time it is executed. Complete the source code in order to print whether the number stored in the variable n is positive or negative.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,17 @@
 #include <stdio.h>
 /**
- * main - to print the size of the data types
+ * main - to print the size of the types
  *
- *Return: always (0)
+ *Return: (0)
  *
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
-
-	printf("size of char: %lu byte(s)\n", sizeof(a));
-	printf("size of intr: %lu byte(s)\n", sizeof(b));
-	printf("size of long intr: %lu byte(s)\n", sizeof(c));
-	printf("size of long long intr: %lu byte(s)\n", sizeof(d));
-        printf("size of floatr: %lu byte(s)\n", sizeof(e));
-	return(0);
+printf("size of char ");
+printf("size of int ");
+printf("size of long ");
+printf("size of long int ");
+printf("size of long long int");
+printf("size of float ");
+return(0);
 }	
