2aa857ee49f40ac36bb714596b912d54c845d87c
paballom <molelekipaballo@gmail.com>
2023-09-04 04:30:02 -0700

    a program that multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,26 +1,39 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 /**
- * main - Prints the multiplication of two numbers, followed by a new line.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
  *
- * Return: If the program receives two arguments - 0.
- *         If the program does not receive two arguments - 1.
+ * Return: the int converted from the string
  */
-int main(int argc, char *argv[])
+int _atoi(char *s)
 {
-int firstNum, secondNum, product;
-if (argc < 3 || argc > 3)
+int i = 0, a = 0, c = 0, leng = 0, d = 0, digit = 0;
+
+while (s[leng] != '\0')
+leng++;
+
+while (i < leng && d == 0)
 {
-printf("Error\n");
-return (0);
+if (s[i] == '-')
+++a;
+
+if (s[i] >= '0' && s[i] <= '9')
+{
+digit = s[i] - '0';
+
+if (a % 2)
+digit = -digit;
+c = c * 10 + digit;
+d = 1;
+
+if (s[i + 1] < '0' || s[i + 1] > '9')
+break;
+d = 0;
 }
-firstNum = atoi(argv[1]);
-seconfNum = atoi(argv[2]);
-product = firstNum * secondSum;
-
-printf("%d\n", product);
-
+i++;
+}
+if (d == 0)
 return (0);
+return (c);
 }
