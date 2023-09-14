b2a951ed1f596876d128169b8dabc397c7101e65
paballom <molelekipaballo@gmail.com>
2023-09-14 11:55:51 -0700

    function that prints strings

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,11 +1,9 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * print_to_98 - Print all natural numbers from n to 98
  * @n: int type number
  */
-
 void print_to_98(int n)
 {
        int i, j;
@@ -30,5 +28,4 @@ void print_to_98(int n)
 					printf("%d\n", j);
 			}
 		}
-	}
-
+}
